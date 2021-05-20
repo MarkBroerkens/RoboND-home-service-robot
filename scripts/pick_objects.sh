e602eb479dcbd854550a90ccf3e38cb4d253f677
Mark Broerkens <broerkens@gmail.com>
2021-05-20 06:19:02 -0700

    feat: automatically navigate to different positions

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/myworld.world" &
-sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$CATKIN_WS/src/RoboND-home-service-robot/worlds/myworld.world" &
+sleep 10
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/$CATKIN_WS/src/RoboND-home-service-robot/maps/myworld_map.yaml  initial_pose_a:=4.71239" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
